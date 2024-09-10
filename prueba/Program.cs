d053a2bd297f9710e2d8fd4425342667f1269fbd
AlexAN <alex.antazo@msn.com>
2024-09-10 11:34:16 +0200

    using System.Drawing;

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+
 Console.BackgroundColor = ConsoleColor.Blue;
 Console.WriteLine("Hello, World!");
 Console.Write("How are you?");
