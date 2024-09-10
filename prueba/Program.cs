93fae177ed6721f99e44467280ba7f6e5b742bc3
AlexAN <alex.antazo@msn.com>
2024-09-10 11:16:49 +0200

    Lorem ipsum

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.BackgroundColor = ConsoleColor.Blue;
 Console.WriteLine("Hello, World!");
+Console.Write("How are you?");
+Console.Write("Lorem ipsum dolor sit amet consectetur adipiscing elit, in fringilla maecenas lobortis duis turpis risus, praesent porta facilisis nostra aptent nibh. Nulla massa taciti vulputate fringilla ac cursus mollis pharetra, penatibus iaculis molestie praesent arcu nibh ridiculus. Donec pharetra vulputate varius felis scelerisque phasellus per aliquam class, viverra dui iaculis urna himenaeos turpis posuere ac, curabitur auctor ultricies neque in duis vivamus habitasse.");
