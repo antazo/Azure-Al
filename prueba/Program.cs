ef40eba5c9f82ee42112e1a8c369c5c4bbad7243
AlexAN <alex.antazo@msn.com>
2024-09-11 11:50:15 +0200

    Correction in Unicode and UTF8

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -35,15 +35,23 @@ Console.Write(terminator);
 // Welcome, this is about artificial intelligence...
 Console.Write("\n\n\u3088\u3046\u3053\u305d\u3001\u3053\u308c\u306f\u4eba\u5de5\u77e5\u80fd\u306b\u3064\u3044\u3066\u3067\u3059\u002e\u002e\u002e\n\t");
 
-// Type your username and press enter
-Console.WriteLine();
-Console.WriteLine("\n\tIntroduce un valor a convertir:");
+// Type any value
+Console.WriteLine("\n\t");
+Console.WriteLine(@"What is your name?");
 
 // Create a string variable and get user input from the keyboard and store it in the variable
-//string userName = Console.ReadLine();
+var anyValue = Console.ReadLine();
+if (anyValue == "")
+    anyValue = "pimpin, no me has dicho como te llamas";
 
 // Print the value of the variable (userName), which will display the input value
-//Console.WriteLine("Username is: " + userName);
+Console.WriteLine(@"Hola " + anyValue + " ^_^");
+
+// Convert F to C
+int fahrenheit = 94;
+decimal celsius = (fahrenheit-32)*(5M/9M);
+
+Console.WriteLine("The temperature is " + celsius + " Celsius");
 
 Console.WriteLine();
 Console.WriteLine();
