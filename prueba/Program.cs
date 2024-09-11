9c1b964ab831beecc4e822385d8b6fa4979e737d
AlexAN <alex.antazo@msn.com>
2024-09-11 11:00:07 +0200

    Unicode and UTF8

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -32,11 +32,18 @@ String terminator = """
 Console.OutputEncoding = Encoding.UTF8;
 Console.Write(terminator);
 
+// Welcome, this is about artificial intelligence...
+Console.Write("\n\n\u3088\u3046\u3053\u305d\u3001\u3053\u308c\u306f\u4eba\u5de5\u77e5\u80fd\u306b\u3064\u3044\u3066\u3067\u3059\u002e\u002e\u002e\n\t");
+
 // Type your username and press enter
-//Console.WriteLine("Enter username:");
+Console.WriteLine();
+Console.WriteLine("\n\tIntroduce un valor a convertir:");
 
 // Create a string variable and get user input from the keyboard and store it in the variable
 //string userName = Console.ReadLine();
 
 // Print the value of the variable (userName), which will display the input value
 //Console.WriteLine("Username is: " + userName);
+
+Console.WriteLine();
+Console.WriteLine();
