ad53842c038d05c9f6f6276a6ceae4de6b969dab
AlexAN <alex.antazo@msn.com>
2024-09-11 14:02:13 +0200

    Console.Clear() added

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 //Console.BackgroundColor = ConsoleColor.Blue;
 //Console.Write("Lorem ipsum dolor sit amet consectetur adipiscing elit, in fringilla maecenas lobortis duis turpis risus, praesent porta facilisis nostra aptent nibh. Nulla massa taciti vulputate fringilla ac cursus mollis pharetra, penatibus iaculis molestie praesent arcu nibh ridiculus. Donec pharetra vulputate varius felis scelerisque phasellus per aliquam class, viverra dui iaculis urna himenaeos turpis posuere ac, curabitur auctor ultricies neque in duis vivamus habitasse.");
+Console.Clear();
 
 String terminator = """
 
