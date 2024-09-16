62c64acb118178f1125abc4e38ec71832e345ca2
AlexAN <alex.antazo@msn.com>
2024-09-16 11:00:40 +0200

    "Desafío para aplicar reglas de negocio"

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -75,7 +75,7 @@ Console.WriteLine();
 Console.WriteLine();
 
 Console.WriteLine("Cálculo del GPA final");
-Console.WriteLine("-----------------------------");
+Console.WriteLine("---------------------");
 // PROJECT PART 1
 
 //Console.WriteLine(FiggleFonts.Slant.Render("Cálculo del GPA final"));
@@ -127,7 +127,7 @@ Console.WriteLine();
 
 
 Console.WriteLine("Desafío de FizzBuzz");
-Console.WriteLine("-----------------------------");
+Console.WriteLine("-------------------");
 
 /*
 Estas son las reglas de FizzBuzz que necesita implementar en el proyecto de código:
@@ -150,9 +150,45 @@ for (int i = 1; i < 101; i++)
         Console.WriteLine($"{i}");
 }
 
+Console.WriteLine();
+Console.WriteLine();
 
+Console.WriteLine("Desafío para aplicar reglas de negocio");
+Console.WriteLine("--------------------------------------");
 
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
+do
+{
+    switch(daysUntilExpiration)
+    {
+        case 0:
+            Console.WriteLine("Your subscription has expired.");
+            break;
+        case 1:
+            Console.WriteLine("Your subscription expires within a day!");
+            discountPercentage = 20;
+            break;
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            Console.WriteLine($"Your subscription will expire in {daysUntilExpiration} days.");
+            discountPercentage = 10;
+            break;
+        default:
+            Console.WriteLine("Your subscription will expire soon. Renew now!");
+            break;
+    }
+    Console.WriteLine($"You have a discount of {discountPercentage}%.");
+    daysUntilExpiration--;
+    Console.WriteLine("Continue? (Y/n)");
+    string pressKey = Console.ReadLine() ?? string.Empty;
+    if (pressKey != null && pressKey.ToLower() == "n")
+        break;
+} while (daysUntilExpiration > 0);
 
 
 Console.WriteLine();
