b2541c03fc9c8f2213d968b97eef279dccac7c78
AlexAN <alex.antazo@msn.com>
2024-09-17 13:48:46 +0200

    "Admin|Manager"

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -379,3 +379,34 @@ Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-2
 
 Console.WriteLine();
 Console.WriteLine();
+
+Console.WriteLine("Heads or Tails");
+Console.WriteLine("--------------");
+
+Console.WriteLine(new Random().Next(0,2) < 1 ? "heads" : "tails");
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Admin|Manager");
+Console.WriteLine("-------------");
+
+
+string permission = "Admin"; //"Admin|Manager"
+int level = 55;
+
+switch (permission)
+{
+    case "Admin":
+        Console.WriteLine( level>55 ? "Welcome, Super Admin user." : "Welcome, Admin user.");
+        break;
+    case "Manager":
+        Console.WriteLine( level>20 ? "Contact an Admin for access." : "You do not have sufficient privileges.");
+        break;
+    default:
+        Console.WriteLine("You do not have sufficient privileges.");
+        break;
+}
+
+Console.WriteLine();
+Console.WriteLine();
