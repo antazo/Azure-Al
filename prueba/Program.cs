21d3d46468a0e7b29e3496635fc298f013a0813b
AlexAN <alex.antazo@msn.com>
2024-09-19 14:00:06 +0200

    "Desafío de aplicar la interpolación de cadenas a una carta de formulario"

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -522,3 +522,83 @@ Console.WriteLine();
 Console.WriteLine();
 
 
+
+Console.WriteLine("Desafío para invertir las palabras de una oración");
+Console.WriteLine("-------------------------------------------------");
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] words = pangram.Split(' ');
+foreach (string word in words)
+{
+    Console.Write(String.Join("", word.ToCharArray().Reverse()) + " ");
+}
+
+Console.WriteLine("\nPangram: " + pangram);
+Console.WriteLine("\nReversed should be: ehT kciuq nworb xof spmuj revo eht yzal god");
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+Console.WriteLine("Desafío para analizar una cadena de pedidos, ordenar los pedidos y etiquetar posibles errores");
+Console.WriteLine("---------------------------------------------------------------------------------------------");
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orders = orderStream.Split(',');
+Array.Sort(orders);
+foreach (string order in orders)
+{
+    Console.Write($"{order}\t");
+    if (order.Length != 4)
+    {
+        Console.Write($" - Error");
+    }
+    Console.Write($"\n");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+Console.WriteLine("Desafío de aplicar la interpolación de cadenas a una carta de formulario");
+Console.WriteLine("------------------------------------------------------------------------");
+/*
+Dear Ms. Barros,
+As a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.
+
+Currently, you own 2,975,000.00 shares at a return of 12.75%.
+
+Our new product, Glorious Future offers a return of 13.13%.  Given your current volume, your potential profit would be ¤63,000,000.00.
+
+Here's a quick comparison:
+
+Magic Yield         12.75%   $55,000,000.00      
+Glorious Future     13.13%   $63,000,000.00
+*/
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+Console.WriteLine(@"Dear {customerName} ,");
+
+
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+// Your logic here
+
+Console.WriteLine(comparisonMessage);
+
+Console.WriteLine();
+Console.WriteLine();
