7e07f53e88b95c972ab125833e33aebafa1e90dd
AlexAN <alex.antazo@msn.com>
2024-10-04 13:36:28 +0200

    GenerateCalculator unit testing complete

--- pruebaTest/CalculatorTest.cs
+++ pruebaTest/CalculatorTest.cs
@@ -4,20 +4,6 @@ namespace prueba;
 public class CalculatorTest
 {
     [TestMethod]
-    public void TestOperation()
-    {
-        // Arrange
-        double a = 10;
-        double b = 5;
-        double expected = 15;
-
-        // Act
-        double actual = Calculator.Add(a, b);
-
-        // Assert
-        Assert.AreEqual(expected, actual);
-    }
-    [TestMethod]
     public void TestAdd()
     {
         // Arrange
@@ -31,8 +17,9 @@ public class CalculatorTest
         // Assert
         Assert.AreEqual(expected, actual);
     }
+
     [TestMethod]
-    public void TestSubstract()
+    public void TestSubtract()
     {
         // Arrange
         double a = 10;
@@ -41,6 +28,64 @@ public class CalculatorTest
 
         // Act
         double actual = Calculator.Subtract(a, b);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void TestMultiply()
+    {
+        // Arrange
+        double a = 10;
+        double b = 5;
+        double expected = 50;
+
+        // Act
+        double actual = Calculator.Multiply(a, b);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void TestDivide()
+    {
+        // Arrange
+        double a = 10;
+        double b = 5;
+        double expected = 2;
+
+        // Act
+        double actual = Calculator.Divide(a, b);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void TestDivideByZero()
+    {
+        // Arrange
+        double a = 10;
+        double b = 0;
+
+        // Act
+        double actual = Calculator.Divide(a, b);
+
+        // Assert
+        Assert.AreEqual(double.NaN, actual);
+    }
+    [TestMethod]
+    public void TestOperation()
+    {
+        // Arrange
+        double a = 10;
+        double b = 5;
+        double expected = 15;
+
+        // Act
+        double actual = Calculator.Add(a, b);
 
         // Assert
         Assert.AreEqual(expected, actual);
